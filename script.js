11c2cb3e49e6d50d902a7b007a60a9a4ad6efdb0
MikChanna <channa.mik@gmail.com>
2020-06-11 20:53:08 -0400

    added DOM elements and assigned array of functions to object

--- script.js
+++ script.js
@@ -9,6 +9,13 @@
 //   passwordText.value = password;
 // }
 
+// DOM Elements
+var upperEl = document.querySelector(".upper");
+var lowerEl = document.querySelector(".lower");
+var numberEl = document.querySelector(".number");
+var specialEl = document.querySelector(".special");
+var generateEl = document.querySelector("#generate");
+
 // slider function
 var slider = document.getElementById("myRange");
 var output = document.querySelector("#demo");
@@ -36,41 +43,26 @@ function getRandomUpper() {
 function getRandomSpecial() {
   return specialChar[Math.floor(Math.random() * specialChar.length)];
 }
-function getrandomNum() {
+function getRandomNum() {
   return [Math.floor(Math.random() * 10)];
 }
 
+// console.log(
+//   uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]
+// );
+// console.log(
+//   lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]
+// );
+// console.log(specialChar[Math.floor(Math.random() * specialChar.length)]);
+
+// console.log([Math.floor(Math.random() * 10)]);
+
+var randomFunctions = {
+  lower: getRandomLower(),
+  upper: getRandomUpper(),
+  special: getRandomSpecial(),
+  num: getRandomNum(),
+};
+
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
-// check to see if the arrays are recognized
-
-console.log(
-  uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]
-);
-console.log(
-  lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]
-);
-console.log(specialChar[Math.floor(Math.random() * specialChar.length)]);
-
-console.log([Math.floor(Math.random() * 10)]);
-
-// random selection
-
-for (var i = 0; i < passwordLength; i++) {
-  var lowercaseRandom =
-    lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
-  var uppercaseRandom =
-    uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
-  var numRandom = Math.floor(Math.random() * 10);
-  var specialRandom =
-    specialChar[Math.floor(Math.random() * specialChar.length)];
-
-  console.log(lowercaseRandom + uppercaseRandom + numRandom + specialRandom);
-}
-
-function genPassword() {
-  for (var i = 0; i < passwordLength; i++) {
-    console.log();
-  }
-}
